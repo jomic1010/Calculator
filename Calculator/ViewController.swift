bd57a17a4f9f7ceca18fda4ffac70c5fe72d3d41
JomiC <jomic1010@naver.com>
2020-07-24 21:52:07 +0900

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by JomiC on 2020/07/24.
+//  Copyright © 2020 JomiC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var result: UILabel!
+    
+    @IBOutlet weak var reset: UIButton!
+    @IBOutlet weak var sign: UIButton!
+    @IBOutlet weak var leftBracket: UIButton!
+    @IBOutlet weak var rightBracket: UIButton!
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var minus: UIButton!
+    @IBOutlet weak var dot: UIButton!
+    @IBOutlet weak var percentage: UIButton!
+    @IBOutlet weak var multiply: UIButton!
+    @IBOutlet weak var division: UIButton!
+    
+    @IBOutlet weak var num0: UIButton!
+    @IBOutlet weak var num1: UIButton!
+    @IBOutlet weak var num2: UIButton!
+    @IBOutlet weak var num3: UIButton!
+    @IBOutlet weak var num4: UIButton!
+    @IBOutlet weak var num5: UIButton!
+    @IBOutlet weak var num6: UIButton!
+    @IBOutlet weak var num7: UIButton!
+    @IBOutlet weak var num8: UIButton!
+    @IBOutlet weak var num9: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
